c63968a881db7f54f82cb6c9ea6a702d5e4dc49f
RafidMuhymin <rafidmuhymin@gmail.com>
2021-06-23 10:29:21 -0700

    minimal repro

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+import logo from "../../static/logo.svg"
+
+export default function Home() {
+  return (
+    <div>
+      Hello world!
+      <img src={logo} alt="The Muhymin Logo" />
+    </div>
+  )
+}
